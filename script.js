094184bce5faacfa46de2a4220751b5471843d7d
adtpdn <adtpdn@gmail.com>
2024-09-17 12:30:35 +0800

    update + add mobile version

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
-// Get the modal
-var modal = document.getElementById("modal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("exampleBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+function openModal() {
+    document.getElementById('modalOverlay').style.display = 'block';
+    document.body.style.overflow = 'hidden';
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+function closeModal() {
+    document.getElementById('modalOverlay').style.display = 'none';
+    document.body.style.overflow = 'auto';
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// Close modal when clicking outside of it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+    if (event.target == document.getElementById('modalOverlay')) {
+        closeModal();
+    }
 }
